74c13d0791bd53ba172c1d71b0f691e5bacecfb4
Kossi Fioklou <kossifioklou10@gmail.com>
2022-12-04 16:23:22 -0800

    Update app.jsx content

--- promptme/src/App.jsx
+++ promptme/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">
-      Hello world!
+      Wondering if you should leave your recruiter before they ghost you?
       </h1>
     </div>
   )
