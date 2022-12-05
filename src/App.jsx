6fb4f74cd2fc8011943cc5ddabe51f0b111d54f2
Kossi Fioklou <kossifioklou10@gmail.com>
2022-12-05 13:10:07 -0800

    Style Home page

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,13 @@ function App() {
 
   return (
     <div className="App">  <Header />
-      <h1 className="text-3xl">
+    <div className="text-3xl flex flex-col items-center justify-center h-screen w-screen">
+      <h1>
       Wondering if you should leave your recruiter before they ghost you?
-      </h1> <Button className=' w-2/3'> Take our short assessment</Button>
+      </h1> <Button className='w-1/4 py-4 m-8'> Take our short assessment</Button>
+      </div>
     </div>
+
   )
 }
 
