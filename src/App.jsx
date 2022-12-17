dc6dc2a6e0d446bcea8b6189877281956c45b537
Kossi Fioklou <kossifioklou10@gmail.com>
2022-12-17 05:02:36 -0800

    Setup route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import About from './components/pages/About';
+import Faqs from './components/pages/Faqs';
 import Button from './components/elements/Button'
 import Header from './components/elements/Header'
 import Login from './components/pages/Login'
@@ -8,11 +11,19 @@ import Signup from './components/pages/Signup'
 function App() {
 
   return (
-    <div className="App">  <Header />
+    <div className="App">
+      <Router>
+      <Header/>
+          <Routes>
+             <Route path='about' element={<About/>}/>
+             <Route path='faqs' element={<Faqs/>}/>
+          </Routes>
+    </Router>
     <div className="text-3xl flex flex-col items-center justify-center h-screen w-screen">
       <h1>
-      Wondering if you should leave your recruiter before they ghost you?
-      </h1> <Button className='w-1/4 py-4 m-8'> Take our short assessment</Button>
+      Wondering if you should leave your <br></br> recruiter before they ghost you?
+      </h1> 
+      <Button className='w-2/4 py-4 m-8'> Take our short assessment</Button>
       </div>
     </div>
 
--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -3,22 +3,22 @@ import Button from '../elements/Button'
 
 function Login() {
   return (
-    <div class="flex flex-col w-full max-w-md px-4 py-8 bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10">
-    <div class="self-center mb-6 text-xl font-light sm:text-2xl ">
+    <div className="flex flex-col w-full max-w-md px-4 py-8 bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10">
+    <div className="self-center mb-6 text-xl font-light sm:text-2xl ">
     Login
     </div>
-    <div class="mt-8">
+    <div className="mt-8">
         <form action="#" autoComplete="on">
-                <div class="flex flex-col mb-2">
-                    <div class="flex relative ">
+                <div className="flex flex-col mb-2">
+                    <div className="flex relative ">
 
-                        <input type="text" id="sign-in-email" class=" rounded-md flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent" placeholder="Email"/>
+                        <input type="text" id="sign-in-email" className=" rounded-md flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent" placeholder="Email"/>
                         </div>
                     </div>
-                <div class="flex flex-col mb-6">
-                    <div class="flex relative ">
+                <div className="flex flex-col mb-6">
+                    <div className="flex relative ">
 
-                        <input type="password" id="sign-in-email" class=" rounded-md flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-gray text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent" placeholder="Your password"/>
+                        <input type="password" id="sign-in-email" className=" rounded-md flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-gray text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent" placeholder="Your password"/>
                         </div>
                     </div>
                     <div >
@@ -28,7 +28,7 @@ function Login() {
                 </form>
                 </div>
             <div className=' flex flex-row items-center text-center justify-between text-base m-2 text-[#8C8CA2]'>
-                <hr class=" w-5/12"/>OR <hr class=" w-5/12"/>
+                <hr className=" w-5/12"/>OR <hr className=" w-5/12"/>
             </div>
             <div className='flex flex-row justify-between m-2' >
                 <div className='w-5/12'>
@@ -39,13 +39,13 @@ function Login() {
                         </div> 
                         
                     </div>
-            <div class="flex items-center justify-center mt-6">
-                <a href="#" target="_blank" class="inline-flex items-center text-sm bold text-center text-[#8C8CA2] hover:text-gray-700 dark:text-gray-100 dark:hover:text-white">
-                    <span class="ml-2">
+            <div className="flex items-center justify-center mt-6">
+                <Link to="/" target="_blank" className="inline-flex items-center text-sm bold text-center text-[#8C8CA2] hover:text-gray-700 dark:text-gray-100 dark:hover:text-white">
+                    <span className="ml-2">
                     By signing in to promptme, you agree to our Terms and Privacy Policy.
 
                     </span>
-                </a>
+                </Link>
             </div>
         </div>
 
