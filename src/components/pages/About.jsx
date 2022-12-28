8200037ae31143dfbe5c18cd7c67d701e7719653
Kossi Fioklou <kossifioklou10@gmail.com>
2022-12-28 09:41:31 -0800

    Add about to home page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,6 +1,8 @@
 const About = () => {
   return (
-    <div>About</div>
+    <div className="text-3xl flex flex-col items-center justify-center h-screen w-screen">
+      <Link to="/Dashboard"><Button type="button">About</Button></Link>
+    </div>
   )
 }
 
--- src/components/pages/Faqs.jsx
+++ src/components/pages/Faqs.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Faqs = () => {
   return (
-    <div>Faqs</div>
+    <div className="text-3xl flex flex-col items-center justify-center h-screen w-screen">
+      <Link to="/Dashboard"><Button type="button">Faqs</Button></Link>
+    </div>
   )
 }
 
