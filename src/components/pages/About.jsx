35e70da4b7af61416c13d76b1581246ae42ffa3c
Kossi Fioklou <kossifioklou10@gmail.com>
2022-12-28 09:59:04 -0800

    Style About page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,8 +1,10 @@
+import React from "react";
+import {} from "react-router-dom";
+
 const About = () => {
   return (
     <div className="text-3xl flex flex-col items-center justify-center h-screen w-screen">
-      <Link to="/Dashboard"><Button type="button">About</Button></Link>
-    </div>
+      About</div>
   )
 }
 
--- src/components/pages/Faqs.jsx
+++ src/components/pages/Faqs.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 const Faqs = () => {
   return (
-    <div className="text-3xl flex flex-col items-center justify-center h-screen w-screen">
-      <Link to="/Dashboard"><Button type="button">Faqs</Button></Link>
-    </div>
+    <div className="text-3xl flex flex-col items-center justify-center h-screen w-screen">Faqs</div>
   )
 }
 
