11c88caaaac3c51fde535913fa3a8585b3dc20c8
Thermiee <navyblack16@yahoo.com>
2023-01-04 10:34:01 +0100

    Fix Signup page

--- src/components/pages/Signup.jsx
+++ src/components/pages/Signup.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { media } from '../../assets/assets'
 import Button from '../elements/Button'
+import Header from '../elements/Header'
 
 function Signup() {
   return (
+    <><Header></Header>
     <section className="flex justify-center items-center h-full">
-      <div className="flex flex-col w-full max-w-md px-4 py-8 bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10">
+      <div className="flex flex-col w-full max-w-md px-4 py-8 bg-white rounded-lg shadow sm:px-6 md:px-8 lg:px-10">
         <div className="self-center mb-6 text-xl font-light text-gray-600 sm:text-2xl ">
           Create your profile
         </div>
@@ -33,23 +35,23 @@ function Signup() {
         <div className='flex flex-row items-center text-center justify-between text-base m-2 text-[#8C8CA2]'>
           <hr className="w-5/12" />OR <hr className=" w-5/12" />
         </div>
-        <div className='flex flex-row justify-between items-center m-2' >
+        <div className='flex flex-row justify-between items-center m-2'>
           <Link to="#">
-            <img className="w-8 h-8" src={media.facebook} alt="facebook" />
+            <img className="h-10" src={media.facebook} alt="facebook" />
           </Link>
           <Link to="#">
             <img className="h-10" src={media.google} alt="facebook" />
           </Link>
         </div>
         <div className="flex items-center justify-center mt-6">
-          <Link to="/" target="_blank" className="inline-flex items-center text-sm bold text-center text-[#8C8CA2] hover:text-gray-700 dark:text-gray-100 dark:hover:text-white">
+          <div className="inline-flex items-center text-sm bold text-center text-[#8C8CA2] hover:text-gray-700">
             <span className="ml-2">
               By signing in to promptme, you agree to our Terms and Privacy Policy.
             </span>
-          </Link>
+          </div>
         </div>
       </div>
-    </section>
+    </section></>
   )
 }
 
